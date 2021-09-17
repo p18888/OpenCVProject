a7e212bcf75f17be301741c90f6197cf1dfc7586
p18888 <77534300+p18888@users.noreply.github.com>
2021-09-17 20:24:34 +0530

    Update asgi.py

--- OpenCV_Pro/asgi.py
+++ OpenCV_Pro/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Rating_Prediction.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OpenCV_Pro.settings')
 
 application = get_asgi_application()
